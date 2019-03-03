b9e41e299aedd6015f238903cc4960a1d5d786dd
Jose P Valle <jpvalle@ucsd.edu>
2019-03-03 01:25:49 -0800

    fixed connection issues to database

--- adminSite/table_dump.php
+++ adminSite/table_dump.php
@@ -3,29 +3,30 @@ function table_dump($table_name)
 {
     /* Database Connection Infromation */
     $db_host = '157.230.150.204';
-    $db_user = 'jpvalle';
-    $db_pswd = 'Coolness1!';
+    $db_user = 'reporter';
+    $db_pswd = 'Reportingpassword1!';
     $db_name = 'bakery_db';
     /* End of database connection info */
 
     // attempt database connection
-    if (!mysql_connect($db_host, $db_user, $db_pswd)) {
+    $link = mysqli_connect($db_host, $db_user, $db_pswd, $db_name);
+    if (mysqli_connect_errno()) {
         die('Cannot connect to database');
     }
 
     // attempt selecting the database
-    if (!mysql_select_db($db_name)) {
+    if (!mysqli_select_db($link, $db_name)) {
         die('Cannot select ' . $db_name);
     }
 
     // attempt querying the entire database
-    $result = mysql_query("SELECT * FROM {$table_name}");
+    $result = mysqli_query($link, "SELECT * FROM {$table_name}");
     if (!$result) {
         die('query failed');
     }
 
     // get the count of the fields of the databse
-    $num_fields = mysql_num_fields($result);
+    $num_fields = mysqli_num_fields($result);
 
     // begin printing the table
     echo "<h1>{$table_name}</h1>";
@@ -36,7 +37,7 @@ function table_dump($table_name)
 
     // loop over the fields to set the table header
     for ($i = 0; $i < $num_fields; $i++) {
-        $field = mysql_fetch_field($result);
+        $field = mysqli_fetch_field($result);
         echo "<td>{$field->name}</td>";
     }
     echo "</tr>\n";
@@ -46,7 +47,7 @@ function table_dump($table_name)
     $empty = true;
 
     // loop over the rows in the query
-    while ($row = mysql_fetch_row($result)) {
+    while ($row = mysqli_fetch_row($result)) {
         $empty = false;// it's not empty
         echo "<tr>";
         //loop over the fields to add to the table
